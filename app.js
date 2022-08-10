51f696ae21e6325e07fe054a09797c4c03653219
Leo Smith <leosmith36@yahoo.com>
2022-08-09 20:31:15 -0500

    Updated positioning

--- app.js
+++ app.js
@@ -1,107 +1,19 @@
-import {makeGraph} from "./graph.js";
-
-let canvas = document.getElementById("chart");
-let context = canvas.getContext("2d");
-
-const WIDTH = 700;
-const HEIGHT = 400;
-
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-
-const TOP = 50, BOTTOM = HEIGHT - 50, LEFT = 125, RIGHT = WIDTH - 50;
-const HRANGE = RIGHT - LEFT, VRANGE = BOTTOM - TOP;
-
-const xLabel = "Applied Potential (V)", yLabel = "Current (A)";
+import {update} from "./canvas.js";
 
 update();
 
-function getMin(array){
-    let length = array.length;
-    let min = array[0];
-    for (let i = 1; i < length; i++){
-        if (array[i] < min){
-            min = array[i]
-        }
-    }
-    return min;
-}
-
-function getMax(array){
-    let length = array.length;
-    let max = array[0];
-    for (let i = 1; i < length; i++){
-        if (array[i] > max){
-            max = array[i]
-        }
-    }
-    return max;
-}
-
-function update(){
-
-    context.clearRect(0, 0, WIDTH, HEIGHT);
-
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.moveTo(LEFT, TOP);
-    context.lineTo(LEFT, BOTTOM);
-    context.moveTo(LEFT, TOP + VRANGE / 2);
-    context.lineTo(RIGHT, TOP + VRANGE / 2)
-    context.stroke();
-
-    let v = document.getElementById("v").value;
-    let e0 = document.getElementById("e0").value;
-
-    let values = makeGraph(v, e0);
-    let xValues = values[0], yValues = values[1];
-
-    let xMin = getMin(xValues), xMax = getMax(xValues);
-    let yMin = getMin(yValues), yMax = getMax(yValues);
-
-    let xRange = xMax - xMin, yRange = yMax - yMin;
-
-    let xLength = xValues.length, yLength = yValues.length;
-
-    let xInc = HRANGE / xRange, yInc = VRANGE / yRange;
-
-    let numberTicks = 10;
-
-    let xTick = xRange / numberTicks, yTick = yRange / numberTicks;
-
-    let yTickLength = VRANGE / numberTicks, xTickLength = HRANGE / numberTicks;
-
-    for (let i = 0; i <= numberTicks; i++){
-        let xTickLabel = ((numberTicks - i) * xTick + xMin).toFixed(3), yTickLabel = (i * yTick + yMin).toPrecision(3);
-        context.fillText((xTickLabel).toString(), LEFT + i * xTickLength + 2, TOP + (VRANGE / 2) + 15);
-        context.fillText((yTickLabel).toString(), LEFT - 40, BOTTOM - i * yTickLength + 2);
-    }
-
-    context.beginPath();
-    context.strokeStyle = "green";
-    context.moveTo(
-        RIGHT - xInc * (xValues[0] - xMin),
-        BOTTOM - yInc * (yValues[0] - yMin)
-    );
-    for (let i = 1; i < xLength; i++){
-        context.lineTo(
-            RIGHT - xInc * (xValues[i] - xMin),
-            BOTTOM - yInc * (yValues[i] - yMin)
-        );
-    }
-    context.stroke();
-
-    context.textAlign = "center";
-    context.fillText(xLabel, LEFT + (HRANGE / 2), BOTTOM + 25);
-    context.fillText(yLabel, LEFT - 80, TOP + (VRANGE / 2));
-}
-
 document.getElementById("button").addEventListener("click", update);
 
-document.getElementById("v").addEventListener("change", function(){
-    document.getElementById("v-num").value = document.getElementById("v").value;
-});
+let sliders = document.getElementsByClassName("input");
+for (let slider of sliders) {
+    slider.addEventListener("change", function(){
+        document.getElementById(slider.id + "-num").value = slider.value;
+    });
+}
 
-document.getElementById("v-num").addEventListener("change", function(){
-    document.getElementById("v").value = document.getElementById("v-num").value;
-})
+let entries = document.getElementsByClassName("input-num");
+for (let entry of entries) {
+    entry.addEventListener("change", function(){
+        document.getElementById(entry.id.substring(0, entry.id.length - 4)).value = entry.value;
+    });
+}
--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-export function makeGraph(nu, e0){
+function makeGraph(nu, e0){
 
     const numberDistanceInc = 50; const numberTimeInc = 200;
     const distanceKeys = Array.from(new Array(numberDistanceInc + 1).keys());
@@ -71,4 +71,95 @@ export function makeGraph(nu, e0){
     let xValues = potentialArray;
     let yValues = currentArray;
     return [xValues, yValues];
+}
+
+function getMin(array){
+    let length = array.length;
+    let min = array[0];
+    for (let i = 1; i < length; i++){
+        if (array[i] < min){
+            min = array[i]
+        }
+    }
+    return min;
+}
+
+function getMax(array){
+    let length = array.length;
+    let max = array[0];
+    for (let i = 1; i < length; i++){
+        if (array[i] > max){
+            max = array[i]
+        }
+    }
+    return max;
+}
+
+export function update(){
+
+    const WIDTH = 700;
+    const HEIGHT = 400;
+
+    const TOP = 50, BOTTOM = HEIGHT - 50, LEFT = 125, RIGHT = WIDTH - 50;
+    const HRANGE = RIGHT - LEFT, VRANGE = BOTTOM - TOP;
+
+    const xLabel = "Applied Potential (V)", yLabel = "Current (A)";
+
+    let canvas = document.getElementById("chart");
+    let context = canvas.getContext("2d");
+
+    context.clearRect(0, 0, WIDTH, HEIGHT);
+    context.textAlign = "center";
+
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.moveTo(LEFT, TOP);
+    context.lineTo(LEFT, BOTTOM);
+    context.moveTo(LEFT, TOP + VRANGE / 2);
+    context.lineTo(RIGHT, TOP + VRANGE / 2)
+    context.stroke();
+
+    let v = document.getElementById("v").value;
+    let e0 = document.getElementById("e0").value;
+
+    let values = makeGraph(v, e0);
+    let xValues = values[0], yValues = values[1];
+
+    let xMin = getMin(xValues), xMax = getMax(xValues);
+    let yMin = getMin(yValues), yMax = getMax(yValues);
+
+    let xRange = xMax - xMin, yRange = yMax - yMin;
+
+    let xLength = xValues.length, yLength = yValues.length;
+
+    let xInc = HRANGE / xRange, yInc = VRANGE / yRange;
+
+    let numberTicks = 10;
+
+    let xTick = xRange / numberTicks, yTick = yRange / numberTicks;
+
+    let yTickLength = VRANGE / numberTicks, xTickLength = HRANGE / numberTicks;
+
+    for (let i = 0; i <= numberTicks; i++){
+        let xTickLabel = ((numberTicks - i) * xTick + xMin).toFixed(3), yTickLabel = (i * yTick + yMin).toPrecision(3);
+        context.fillText((xTickLabel).toString(), LEFT + i * xTickLength, TOP + (VRANGE / 2) + 15);
+        context.fillText((yTickLabel).toString(), LEFT - 25, BOTTOM - i * yTickLength + 2);
+    }
+
+    context.beginPath();
+    context.strokeStyle = "green";
+    context.moveTo(
+        RIGHT - xInc * (xValues[0] - xMin),
+        BOTTOM - yInc * (yValues[0] - yMin)
+    );
+    for (let i = 1; i < xLength; i++){
+        context.lineTo(
+            RIGHT - xInc * (xValues[i] - xMin),
+            BOTTOM - yInc * (yValues[i] - yMin)
+        );
+    }
+    context.stroke();
+
+    context.fillText(xLabel, LEFT + (HRANGE / 2), BOTTOM + 25);
+    context.fillText(yLabel, LEFT - 80, TOP + (VRANGE / 2));
 }
